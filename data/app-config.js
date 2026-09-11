2070cca25e85105245636c7d86aa6b5af65d2376
Prostarm <prostarm2008@gmail.com>
2026-09-11 20:48:59 +0530

    Update app-config.js

--- data/app-config.js
+++ data/app-config.js
@@ -12,12 +12,12 @@
    header costs nothing.
 ------------------------------------------------------------------ */
 const APP_CONFIG = {
-  flowUrl: '',
+  flowUrl: 'https://8549b42c711be8948675eefb5cf215.dd.environment.api.powerplatform.com:443/powerautomate/automations/direct/cu/05/workflows/bbb91f5798ed4bd9989fbfc2d1ba065d/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=xwRFQTzbnO8kYEiJPhwQKueFx-JhkUmEGXuYyTaozuE',
 
   // Second flow, GET, used only by admin / branch / regional sign-ins to read
   // every engineer's surveys back out of SharePoint. Leave empty and the
   // "My surveys" tab shows only what is on this device.
-  listUrl: '',
+  listUrl: 'https://8549b42c711be8948675eefb5cf215.dd.environment.api.powerplatform.com:443/powerautomate/automations/direct/cu/16/workflows/6292d4da5508453a856e044fb911d3fa/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=9GIxROX-0ONm7wqugwPPKHG7F6iWhk2hVk4INXIqW70',
 
   flowContentType: 'text/plain;charset=UTF-8'
 };
